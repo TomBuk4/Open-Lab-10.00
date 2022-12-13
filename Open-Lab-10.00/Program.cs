25edd1b8e20c870a7197e4b716c4f054b8311263
TomBuk4 <tomikbuk4@gmail.com>
2022-12-13 08:41:36 +0100

    Done 10.01

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -1,7 +1,13 @@
+using Open_Lab_10._00;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Book LOTR = new Book();
+        LOTR.title = "C";
+        Console.WriteLine("LORT.title");
+
     }
 }

98929d421b862fb65840e3d372833541417a49e3
TomBuk4 <tomikbuk4@gmail.com>
2023-01-09 18:32:03 +0100

    Work in progres

--- Open-Lab-10.00/Book.cs
+++ Open-Lab-10.00/Book.cs
@@ -15,39 +15,39 @@ namespace Open_Lab_10._00
         private string category;
         private int releaseDate;
 
-        private string GetTitle() 
-            { return title; }
+        private string GetTitle()
+        { return title; }
         public string SetTitle(string title)
-        { 
+        {
             this.title = title;
             return title;
         }
-        private int GetPages() 
+        private int GetPages()
         { return pages; }
         public int SetPages(int pages)
-        { 
-            if(pages < 0)
-            { 
-                return 1; 
+        {
+            if (pages < 0)
+            {
+                return 1;
             }
-            this.pages = pages; 
+            this.pages = pages;
             return pages;
         }
-        private string GetAuthor() 
+        private string GetAuthor()
         { return author; }
         public string SetAuthor(string author)
-        { 
-            this.author = author; 
+        {
+            this.author = author;
             return author;
         }
-        private string GetCategory() 
+        private string GetCategory()
         { return category; }
         public string SetCategory(string category)
         {
             this.category = category;
             return category;
         }
-        private int GetReleaseDate() 
+        private int GetReleaseDate()
         { return releaseDate; }
         public int SetReleaseDate(int releaseDate)
         {
@@ -70,6 +70,30 @@ namespace Open_Lab_10._00
             Console.WriteLine(category);
             Console.WriteLine(releaseDate);
             Console.WriteLine(pages);
+        }
+        public Book()
+        {
+            title = "-1";
+            pages = -1;
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+        public Book(string title, int pages)
+        {
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+            this.title = title;
+            this.pages = pages;
+        }
+        public Book(string title, int pages, string author, string category, int releaseDate)
+        {
+            this.title = title;
+            this.pages = pages;
+            this.author = author;
+            this.category = category;
+            this.releaseDate = releaseDate;
         }
     }
 }
